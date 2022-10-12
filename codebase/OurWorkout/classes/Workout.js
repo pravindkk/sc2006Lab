06dc607a4b79a6984a47166c415f0ca0e4c64f7c
Adil Hasan <adilhasan927@gmai.com>
2022-10-12 20:47:44 +0800

    Workout class

---
+++ codebase/OurWorkout/classes/Workout.js
@@ -0,0 +1,77 @@
+import { OurObject } from "./OurObject";
+import { User } from "./User";
+
+export class Workout
+{
+    #internalUuid;
+    #user;
+    #nameAkaTitle;
+    #description;
+    #workoutExerciseInfos;
+    #photoUuidsForDb;
+    #likeUsers;
+
+    constructor(obj)
+    {
+        this.#internalUuid = obj.internalUuid;
+        this.#user = obj.user;
+        this.#nameAkaTitle = obj.nameAkaTitle;
+        this.#description = obj.description;
+        this.#workoutExerciseInfos = obj.workoutExerciseInfos;
+        this.#photoUuidsForDb = obj.photoUuidsForDb;
+        this.#likeUsers = obj.likeUsers;
+    }
+
+    toObj()
+    {
+        obj = {};
+        obj.internalUuid = this.internalUuid;
+        obj.#user = this.user;
+        obj.#nameAkaTitle = this.nameAkaTitle;
+        obj.#description = this.description;
+        obj.#workoutExerciseInfos = this.workoutExerciseInfos;
+        obj.#photoUuidsForDb = this.photoUuidsForDb;
+        obj.#likeUsers = this.likeUsers;
+        return obj;
+    }
+
+    getInternalUUid = () => this.#internalUuid;
+    
+    getUser = () => this.#user;
+    setUser = _ => this.#user = _;
+
+    getTitle = () => this.#nameAkaTitle;
+    setTitle = _ => this.#nameAkaTitle = _;
+    getWorkoutName = this.getTitle;
+    setWorkoutName = this.setTitle;
+
+    getDescription = () => this.#description;
+    setDescription = _ => this.#description = _;
+
+    getWorkoutExerciseInfos = () => this.#workoutExerciseInfos;
+    addWorkoutExerciseInfo = _ => this.#workoutExerciseInfos.push(_);
+    deleteWorkoutExerciseInfo = _ => {
+        const idx = this.#workoutExerciseInfos.indexOf(_);
+        if (idx > -1) {
+          array.splice(idx, 1);
+        }
+    };
+
+    getPhotoUuidsForDb = () => this.#photoUuidsForDb;
+    addPhotoUuidForDb = _ => this.#photoUuidsForDb.push(_);
+    deletePhotoUuidForDb = _ => {
+        const idx = this.#photoUuidsForDb.indexOf(_);
+        if (idx > -1) {
+          array.splice(idx, 1);
+        }
+    };
+
+    getLikeUsers = () => this.#likeUsers;
+    addLikeUser = _ => this.#likeUsers.push(_);
+    deleteLikeUser = _ => {
+        const idx = this.#likeUsers.indexOf(_);
+        if (idx > -1) {
+          array.splice(idx, 1);
+        }
+    };
+}
